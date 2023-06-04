76f9ce35876dba8e365bb62984df7de5c5072dcc
Adivaprayoga <prayoga.adiva@gmail.com>
2023-06-05 04:36:29 +0800

    disable button if input is empty

--- src/components/NewTweetForm.tsx
+++ src/components/NewTweetForm.tsx
@@ -2,7 +2,8 @@ import { useSession } from "next-auth/react";
 import { api } from "~/utils/api"
 import { Button } from "./Button";
 import { ProfileImage } from "./ProfileImage";
-import { FormEvent, useCallback, useLayoutEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
+import type { FormEvent } from "react";
 
 function updateTextAreaSize(textArea?: HTMLTextAreaElement) {
     if (textArea == null) return
@@ -20,6 +21,7 @@ export function NewTweetForm() {
 function Form() {
     const session = useSession()
     const [inputValue, setInputValue] = useState("")
+    const [buttonStatus, setButtonStatus] = useState(true)
     const textAreaRef = useRef<HTMLTextAreaElement>()
     const inputRef = useCallback((textArea: HTMLTextAreaElement) => {
         updateTextAreaSize(textArea);
@@ -31,32 +33,44 @@ function Form() {
     }, [inputValue])
 
     const createTweet = api.tweet.create.useMutation({ onSuccess: newTweet => {
-        console.log(newTweet)
         setInputValue("")
     }})
 
     if (session.status !== "authenticated") return null
 
-    function handleSubmit(e: FormEvent) {
-        e.preventDefault()
+    const handleChange = (event: any) => {
+        event.preventDefault()
 
+        setInputValue(event.target.value);
+
+        console.log(inputValue)
+
+        if(event.target.value.trim().length > 0) {
+            setButtonStatus(false)
+        }else {
+            setButtonStatus(true)
+        }
+
+    };
+
+    function handleSubmit() {
         createTweet.mutate({ content: inputValue })
     }
         
      return (
-        <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-2">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-2 border-b px-4 py-4">
             <div className="flex gap-4">
                 <ProfileImage src={session.data.user.image} />
                 <textarea 
                     ref={inputRef}
                     style={{ height: 0 }}
                     value={inputValue}
-                    onChange={e => setInputValue(e.target.value)}
+                    onChange={e => handleChange(e)}
                     className="flex-grow resize-none overflow-hidden p-4 text-lg outline-none" 
                     placeholder="What's happening?">
                 </textarea>
             </div>
-            <Button className="self-end">Tweet</Button>
+            <Button className="self-end" disabled={buttonStatus} >Tweet</Button>
         </form>
      );
 }
