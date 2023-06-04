1f8b6e2bd5d6321ad23de18992da4371a413df01
Adivaprayoga <prayoga.adiva@gmail.com>
2023-06-05 04:36:49 +0800

    add custom container width

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,25 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '640px',
+          },
+          '@screen md': {
+            maxWidth: '768px',
+          },
+          '@screen lg': {
+            maxWidth: '1024px',
+          },
+          '@screen xl': {
+            maxWidth: '1280px',
+          },
+        }
+      })
+    }
+  ],
 } satisfies Config;
