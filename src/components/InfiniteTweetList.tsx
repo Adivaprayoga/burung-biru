23a7f01ef3580d43c13a5c4b52993a6d89f520a0
Adivaprayoga <prayoga.adiva@gmail.com>
2023-06-04 23:07:46 +0800

    add heart button component for recent tweet

--- src/components/InfiniteTweetList.tsx
+++ src/components/InfiniteTweetList.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link"
 import InfiniteScroll from "react-infinite-scroll-component"
 import { ProfileImage } from "./ProfileImage"
-import { create } from "domain"
+import { useSession } from "next-auth/react"
+import { VscHeart, VscHeartFilled } from "react-icons/vsc"
+import { IconHoverEffect } from "./IconHoverEffect"
 
 type Tweet = {
     id: string
@@ -62,6 +64,46 @@ function TweetCard({ id, user, content, createdAt, likeCount, likedByMe} : Tweet
                 <span className="text-gray-500">{dateTimeFormatter.format(createdAt)}</span>
             </div>
             <p className="whitespace-pre-wrap">{content}</p>
+            <HeartButton likedByMe={likedByMe} likeCount={likeCount} />
         </div>
     </li>
+}
+
+type HeartButtonProps = {
+    likedByMe: boolean;
+    likeCount: number;
+}
+
+function HeartButton({ likedByMe, likeCount } : HeartButtonProps ){
+    const session = useSession()
+    const HeartIcon = likedByMe ? VscHeartFilled : VscHeart
+
+    if (session.status != "authenticated") {
+        return (
+            <div className="mb-1 mt-2 flex items-center gap-3 self-start text-gray-500">
+                <HeartIcon />
+                <span>{likeCount}</span>
+            </div>
+        );
+    }
+
+    return (
+        <button className={`group items-center gap-1 self-start flex transition-colors duration-200 ${
+            likedByMe 
+                ? "text-red-500" 
+                : "text-gray-500 hover:text-red-500 focus-visible:text-red-500"
+            }`}
+        >
+            <IconHoverEffect red>
+                <HeartIcon className={`trasition-colors duration-200 ${
+                    likedByMe 
+                        ? "fill-red-500" 
+                        : "fill-gray-500 group-hover:fill-red-500 group-focus-visible:fill-red-500"
+                    }`} 
+                />
+            </IconHoverEffect>
+
+            <span>{likeCount}</span>
+        </button> 
+    );
 }
