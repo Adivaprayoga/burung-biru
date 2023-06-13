2620e71c0fa9879c5a7fced5b7cc15c6a286a1ce
Adivaprayoga <prayoga.adiva@gmail.com>
2023-06-13 20:57:16 +0800

    show profile tweets logic

--- src/server/api/routers/tweet.ts
+++ src/server/api/routers/tweet.ts
@@ -10,6 +10,24 @@ import {
 } from "~/server/api/trpc";
 
 export const tweetRouter = createTRPCRouter({
+  // Show profile tweets
+  infiniteProfileFeed: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+        limit: z.number().optional(),
+        cursor: z.object({ id: z.string(), createdAt: z.date() }).optional(),
+      })
+    )
+    .query(async ({ input: { limit = 10, userId, cursor }, ctx }) => {
+      return await getInfiniteTweets({
+        limit,
+        ctx,
+        cursor,
+        whereClause: { userId },
+      });
+    }),
+
   // Show tweets
   infiniteFeed: publicProcedure
     .input(
