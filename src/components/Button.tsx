83828d84922a6a3f3c828dc909864c390b6a282e
Adivaprayoga <prayoga.adiva@gmail.com>
2023-06-19 19:13:30 +0800

    update button style

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,20 +4,24 @@ type ButtonProps = {
   small?: boolean;
   gray?: boolean;
   className?: string;
+  tweet?: boolean;
 } & DetailedHTMLProps<
   ButtonHTMLAttributes<HTMLButtonElement>,
   HTMLButtonElement
 >;
 
 export function Button({ small, gray, className = "", ...props }: ButtonProps) {
-  const sizeClasses = small ? "px-2 py-1" : "px-4 py-2 font-bold";
+  const sizeClasses = small ? "py-1" : "py-2 font-bold";
   const colorClasses = gray
-    ? "bg-gray-400 hover:bg-gray-300 focus-visible:bg-gray-300"
-    : "bg-sky-500 hover:bg-sky-400 focus-visible:bg-sky-400";
+    ? "border-gray-400 bg-white text-black border hover:bg-red-100 hover:border-red-600 hover:text-red-600 font-bold"
+    : "bg-sky-500 hover:bg-sky-400 focus-visible:bg-sky-100 font-bold";
+
   return (
-    <button
-      className={`rounded-full text-white transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
-      {...props}
-    ></button>
+    <>
+      <button
+        className={`w-32 rounded-full text-white transition-colors duration-200 disabled:cursor-not-allowed disabled:opacity-50 ${sizeClasses} ${colorClasses} ${className}`}
+        {...props}
+      ></button>
+    </>
   );
 }
